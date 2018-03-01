842c15d91173c2b6e671b9d45578f28e94db2b53
Thomas Henderson <tom@mathpunk.net>
2018-03-01 16:05:58 -0600

    reader, reader_test: Read files and parse the data into maps

--- src/certainty_derivative/reader.clj
+++ src/certainty_derivative/reader.clj
@@ -4,7 +4,8 @@
             [clojure.spec.alpha :as s]))
 
 (defn detect-delimiter
-  "Using the assumption that delimiter characters do not appear in the data values themselves."
+  "Using the assumption that delimiter characters do not appear in the data
+  values themselves."
   [input-row]
   (cond (string/includes? input-row "|") :pipe
         (string/includes? input-row ",") :comma
@@ -24,10 +25,27 @@
   [input-row]
   (string/split input-row #" \| "))
 
+;; NB: It seems like there's probably a way to compress these methods into one
+;; method with a regex with an 'or' expression. The first solution I tried in
+;; that manner didn't work like I expected, hence the above. It seems clear
+;; enough if a little redundant.
+
 (defn parse-row [input-row]
   (let [fields (tokenize-row input-row)
-        column-names [:last-name :first-name :gender :favorite-color :date-of-birth]]
+        column-names [:last-name :first-name :gender
+                      :favorite-color :date-of-birth]]
     (zipmap column-names fields)))
 
 (s/def :certainty-derivative.input/row
-  (s/keys :req-un [::last-name ::first-name ::gender ::favorite-color ::date-of-birth]))
+  (s/keys :req-un [::last-name ::first-name ::gender
+                   ::favorite-color ::date-of-birth]))
+
+(defn read-file [filename]
+  (let [acc (transient [])]
+    (with-open [r (clojure.java.io/reader filename)]
+      (doseq [line (line-seq r)]
+        (conj! acc (parse-row line))))
+    (persistent! acc)))
+
+(defn read-files [& filenames]
+  (mapcat parse-file filenames))
--- test/certainty_derivative/reader_test.clj
+++ test/certainty_derivative/reader_test.clj
@@ -1,5 +1,6 @@
 (ns certainty-derivative.reader-test
   (:require  [certainty-derivative.reader :refer :all]
+             [certainty-derivative.generator :refer [generate-test-data]]
              [clojure.spec.alpha :as s]
              [clojure.string :as string]
              [clojure.test :refer [deftest testing is]]
@@ -38,3 +39,19 @@
   (is (s/valid? :certainty-derivative.input/row (parse-row example-space-row)))
   (is (s/valid? :certainty-derivative.input/row (parse-row example-comma-row)))
   (is (s/valid? :certainty-derivative.input/row (parse-row example-pipe-row))))
+
+(deftest test-file-reading
+  (do
+    (generate-test-data 100)
+    (is (= 100 (count (read-file "./resources/001.txt"))))
+    (is (= 300 (count (read-files "./resources/001.txt"
+                                  "./resources/002.txt"
+                                  "./resources/003.txt"))))))
+
+(deftest test-file-parsing
+  (do
+    (generate-test-data 100)
+    (let [data (read-files "./resources/001.txt"
+                           "./resources/002.txt"
+                           "./resources/003.txt")]
+      (is (every? #(s/valid? :certainty-derivative.input/row %) data)))))
