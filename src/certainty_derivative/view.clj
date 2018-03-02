5ec7a84b442bf9d6255b073a9e1ae81056400629
Thomas Henderson <tom@mathpunk.net>
2018-03-01 18:45:30 -0600

    view: Sort by gender, then last name ascending

--- src/certainty_derivative/view.clj
+++ src/certainty_derivative/view.clj
@@ -2,3 +2,7 @@
 
 (defn sort-by-last-name-descending [records]
   (reverse (sort-by :last-name records)))
+
+(defn sort-by-gender-and-last-name-ascending [records]
+  (let [score (fn [gender] (if (= gender "f") 1 0))]
+    (sort-by (juxt (comp - score :gender) :last-name) records)))
--- test/certainty_derivative/view_test.clj
+++ test/certainty_derivative/view_test.clj
@@ -2,7 +2,7 @@
   (:require [certainty-derivative.view :refer :all]
             [clojure.test :refer [deftest testing is]]))
 
-(def sample [{:last-name "Alexander", :first-name "Mya", :gender "f",
+(def sample [{:last-name "Alexander", :first-name "Mya", :gender "Decline to state",
               :favorite-color "green", :date-of-birth "1979-3-23"}
              {:last-name "Henry", :first-name "Giovanni", :gender "m",
               :favorite-color "orange", :date-of-birth "1941-7-16"}
@@ -13,7 +13,26 @@
              {:last-name "Alberts", :first-name "Margaret", :gender "m",
               :favorite-color "purple", :date-of-birth "1902-9-2"}])
 
-(deftest last-name-sort
+(deftest test-last-name-sort
   (let [sorted (sort-by-last-name-descending sample)]
     (is (= "Alberts" ((last sorted) :last-name)))
     (is (= "Wright" ((first sorted) :last-name)))))
+
+(deftest test-gender-sort
+  (let [records [{:gender "m" :last-name "Adams"}
+                 {:gender "f" :last-name "Zorro"}
+                 {:gender "f" :last-name "Marcos"}]
+        more-records [{:gender "Decline to state", :last-name "Adams"}
+                      {:gender "f", :last-name "Wright"}]]
+    (is (= "Marcos" (-> records
+                        sort-by-gender-and-last-name-ascending
+                        first
+                        :last-name)))
+    (is (= "Wright" (-> more-records
+                        sort-by-gender-and-last-name-ascending
+                        first
+                        :last-name)))))
+
+
+
+
