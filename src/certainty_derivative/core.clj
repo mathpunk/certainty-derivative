b6f3932da7c5531cc94f48c7f554c25f317e0702
Thomas Henderson <tom@mathpunk.net>
2018-03-02 16:08:42 -0600

    core: View (unsorted) records from files, given their paths

--- src/certainty_derivative/core.clj
+++ src/certainty_derivative/core.clj
@@ -1,7 +1,16 @@
-(ns certainty-derivative.core)
+(ns certainty-derivative.core
+  (:require [certainty-derivative.loader.read :refer [read-files]]
+            [certainty-derivative.viewer.format :as format]))
 
 (defn -main []
   (println "hello, -main"))
 
+(defn render [records]
+  (doseq [record records]
+    (-> record
+        format/friendly-format
+        println)))
+
 (defn view [& args]
-  (println (str "sorting: " args)))
+  (let [records (apply read-files args)]
+    (render records)))
