3d6c214933eb0f55e3aa0793332817da0dfb9222
Thomas Henderson <tom@mathpunk.net>
2018-03-02 19:58:00 -0600

    sort: Refactor multimethod into case dispatch
    Upon reflection, I realized that it would be easier to dispatch a `--reverse`
    option if the option map were available in the dispatch-sort function. I used
    the multimethod to make sure I could extend to new kinds of searches, but this
    still looks quite open for extension.

--- src/certainty_derivative/viewer/sort.clj
+++ src/certainty_derivative/viewer/sort.clj
@@ -5,22 +5,16 @@
 
 (defn sort-by-gender-and-last-name-ascending [records]
   (let [score (fn [gender] (if (= gender "f") 1 0))]
-    (sort-by (juxt (comp - score :certainty-derivative.record/gender) :certainty-derivative.record/last-name) records)))
+    (sort-by (juxt (comp - score :certainty-derivative.record/gender)
+                   :certainty-derivative.record/last-name)
+             records)))
 
 (defn sort-by-date-of-birth-ascending [records]
   (sort-by :certainty-derivative.record/date-of-birth records))
 
-(defmulti dispatch-sort
-  (fn [options _] (get-in options [:options :sort])))
-
-(defmethod dispatch-sort "women"
-  [options records]
-  (sort-by-gender-and-last-name-ascending records))
-
-(defmethod dispatch-sort "lname"
-  [options records]
-  (sort-by-last-name-descending records))
-
-(defmethod dispatch-sort "dob"
-  [options records]
-  (sort-by-date-of-birth-ascending records))
+(defn dispatch-sort [options records]
+  (let [strategy (get-in options [:options :sort])]
+    (case strategy
+      "women" (sort-by-gender-and-last-name-ascending records)
+      "lname" (sort-by-last-name-descending records)
+      "dob" (sort-by-date-of-birth-ascending records))))
