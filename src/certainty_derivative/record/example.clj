c59f61e9066de42706a40a5f8f6d1fcab809ee3e
Thomas Henderson <tom@mathpunk.net>
2018-03-11 22:40:50 -0500

    Move test data

--- src/certainty_derivative/record/example.clj
+++ src/certainty_derivative/record/example.clj
@@ -25,3 +25,30 @@
                                           example-gender
                                           example-favorite-color
                                           example-date-of-birth]))
+
+
+(def sample-input [{:certainty-derivative.record/last-name "Alexander"
+                    :certainty-derivative.record/first-name "Mya"
+                    :certainty-derivative.record/gender "Decline to state"
+                    :certainty-derivative.record/favorite-color "green"
+                    :certainty-derivative.record/date-of-birth "1979-3-23"},
+                   {:certainty-derivative.record/last-name "Henry",
+                    :certainty-derivative.record/first-name "Giovanni",
+                    :certainty-derivative.record/gender "m",
+                    :certainty-derivative.record/favorite-color "orange",
+                    :certainty-derivative.record/date-of-birth "1941-7-16"},
+                   {:certainty-derivative.record/last-name "Wright",
+                    :certainty-derivative.record/first-name "Camila",
+                    :certainty-derivative.record/gender "f",
+                    :certainty-derivative.record/favorite-color "orange",
+                    :certainty-derivative.record/date-of-birth "1970-3-28"},
+                   {:certainty-derivative.record/last-name "Cunningham",
+                    :certainty-derivative.record/first-name "Paul",
+                    :certainty-derivative.record/gender "f",
+                    :certainty-derivative.record/favorite-color "green",
+                    :certainty-derivative.record/date-of-birth "1995-7-19"},
+                   {:certainty-derivative.record/last-name "Alberts",
+                    :certainty-derivative.record/first-name "Margaret",
+                    :certainty-derivative.record/gender "m",
+                    :certainty-derivative.record/favorite-color "purple",
+                    :certainty-derivative.record/date-of-birth "1902-9-2"}])
--- test/certainty_derivative/viewer/sort_test.clj
+++ test/certainty_derivative/viewer/sort_test.clj
@@ -1,40 +1,15 @@
 (ns certainty-derivative.viewer.sort-test
   (:require [certainty-derivative.loader.transform :refer [string->date]]
+            [certainty-derivative.record.example :refer [sample-input]]
             [certainty-derivative.viewer.sort :refer :all]
             [clojure.test :refer [deftest is testing]]))
-
-(def sample-input [{:certainty-derivative.record/last-name "Alexander"
-                    :certainty-derivative.record/first-name "Mya"
-                    :certainty-derivative.record/gender "Decline to state"
-                    :certainty-derivative.record/favorite-color "green"
-                    :certainty-derivative.record/date-of-birth "1979-3-23"},
-                   {:certainty-derivative.record/last-name "Henry",
-                    :certainty-derivative.record/first-name "Giovanni",
-                    :certainty-derivative.record/gender "m",
-                    :certainty-derivative.record/favorite-color "orange",
-                    :certainty-derivative.record/date-of-birth "1941-7-16"},
-                   {:certainty-derivative.record/last-name "Wright",
-                    :certainty-derivative.record/first-name "Camila",
-                    :certainty-derivative.record/gender "f",
-                    :certainty-derivative.record/favorite-color "orange",
-                    :certainty-derivative.record/date-of-birth "1970-3-28"},
-                   {:certainty-derivative.record/last-name "Cunningham",
-                    :certainty-derivative.record/first-name "Paul",
-                    :certainty-derivative.record/gender "f",
-                    :certainty-derivative.record/favorite-color "green",
-                    :certainty-derivative.record/date-of-birth "1995-7-19"},
-                   {:certainty-derivative.record/last-name "Alberts",
-                    :certainty-derivative.record/first-name "Margaret",
-                    :certainty-derivative.record/gender "m",
-                    :certainty-derivative.record/favorite-color "purple",
-                    :certainty-derivative.record/date-of-birth "1902-9-2"}])
 
 (def sample (map (fn [record]
                    (update-in record
                               [:certainty-derivative.record/date-of-birth]
                               string->date)) sample-input))
 
-(deftest test-last-name-asscending-sort
+(deftest test-last-name-ascending-sort
   (let [expected '("Alberts" "Alexander" "Cunningham" "Henry" "Wright")
         sorted (by-last-name sample)]
     (is (= expected (map :certainty-derivative.record/last-name sorted)))))
